8d84d220d6458d8267c33106baf3706efc9793f1
devibala813 <51949237+devibala813@users.noreply.github.com>
2019-07-03 11:09:32 +0530

    Update 57.py

--- 57.py
+++ 57.py
@@ -1,8 +1,8 @@
-a,b=map(int,input().split())
+a1,b1=map(int,input().split())
 list1=list(map,int(input().split()))
 sum=0
 for i in list1:
-   if(i==b):
+   if(i==b1):
       sum+=1
 print(sum)      
       
